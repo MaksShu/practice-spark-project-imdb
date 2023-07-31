f6a2670f4f3487f913a41ceaa1bc42b1ca15dc4d
MaksShu <54746440+MaksShu@users.noreply.github.com>
2023-07-31 12:50:16 +0300

    various statistics over data

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ spark_session = (SparkSession.builder
 
 # Set paths to datasets files
 names_path = './data/name.basics.tsv.gz'
-basics_path = './data/title.basics.tsv.gz'
+titles_path = './data/title.basics.tsv.gz'
 crew_path = './data/title.crew.tsv.gz'
 ratings_path = './data/title.ratings.tsv.gz'
 
@@ -34,7 +34,7 @@ def create_dataframes():
     names_df = spark_session.read.csv(names_path, schema=names_schema, sep='\t', header=True, nullValue="\\N")
 
     # Create schema for basics dataset
-    basics_schema = t.StructType([t.StructField(title_id, t.StringType(), False),
+    titles_schema = t.StructType([t.StructField(title_id, t.StringType(), False),
                                   t.StructField(title_type, t.StringType(), True),
                                   t.StructField(title_primary, t.StringType(), True),
                                   t.StructField(title_original, t.StringType(), True),
@@ -45,7 +45,7 @@ def create_dataframes():
                                   t.StructField(title_genres, t.StringType(), True)])
 
     # Create basics dataframe from csv file, where separator is \t, columns has headers and null value is \N
-    basics_df = spark_session.read.csv(basics_path, schema=basics_schema, sep='\t', header=True, nullValue="\\N")
+    titles_df = spark_session.read.csv(titles_path, schema=titles_schema, sep='\t', header=True, nullValue="\\N")
 
     # Create schema for crew dataset
     crew_schema = t.StructType([t.StructField(title_id, t.StringType(), False),
@@ -63,23 +63,43 @@ def create_dataframes():
     # Create ratings dataframe from csv file, where separator is \t, columns has headers and null value is \N
     ratings_df = spark_session.read.csv(ratings_path, schema=ratings_schema, sep='\t', header=True, nullValue="\\N")
 
-    return names_df, basics_df, crew_df, ratings_df
+    return names_df, titles_df, crew_df, ratings_df
 
 
-names_df, basics_df, crew_df, ratings_df = create_dataframes()
+names_df, titles_df, crew_df, ratings_df = create_dataframes()
 
-names_df.show()
-names_df.printSchema()
-names_df.describe().show()
 
-basics_df.show()
-basics_df.printSchema()
-basics_df.describe().show()
+def analyse_data():
+    # Show first 20 rows of each dataframe
+    names_df.show()
+    titles_df.show()
+    crew_df.show()
+    ratings_df.show()
 
-crew_df.show()
-crew_df.printSchema()
-crew_df.describe().show()
+    # Show schema of each dataframe
+    names_df.printSchema()
+    titles_df.printSchema()
+    crew_df.printSchema()
+    ratings_df.printSchema()
 
-ratings_df.show()
-ratings_df.printSchema()
-ratings_df.describe().show()
+    # Show rows number of each dataframe
+    print('names:', names_df.count())
+    print('titles:', titles_df.count())
+    print('crew:', crew_df.count())
+    print('ratings:', ratings_df.count())
+
+    # Show statistics for columns of each dataframe
+    names_df.describe().show()
+    titles_df.describe().show()
+    crew_df.describe().show()
+    ratings_df.describe().show()
+
+    # Show statistics about numerical columns
+    names_df.select(name_birth_year, name_death_year).summary().show()
+    titles_df.select(title_adult, title_start_year, title_end_year, title_minutes).summary().show()
+    ratings_df.select(ratings_average, ratings_votes).summary().show()
+
+
+analyse_data()
+
+
